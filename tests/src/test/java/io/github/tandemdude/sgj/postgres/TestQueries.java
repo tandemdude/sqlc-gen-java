b2bc641442878433abcbe7f62dff60825ef7a227
thomm.o <43570299+tandemdude@users.noreply.github.com>
2025-02-03 16:55:42 +0000

    chore: silence maven, try to fix test not running

--- tests/src/test/java/io/github/tandemdude/sgj/postgres/TestQueries.java
+++ tests/src/test/java/io/github/tandemdude/sgj/postgres/TestQueries.java
@@ -1,20 +1,18 @@
 package io.github.tandemdude.sgj.postgres;
 
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Testcontainers
-public class QueriesTest {
+public class TestQueries {
     @Container
     private final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest")
             .withInitScript("postgres/schema.sql");
